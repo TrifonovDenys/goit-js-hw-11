c81b9dfd0d6f27a82e1df2bd7cf51037f59c3932
Trifonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-05-30 12:02:54 +0300

    async await

--- src/index.js
+++ src/index.js
@@ -24,52 +24,50 @@ function onLoadMore() {
   LoadMore(word, page);
 }
 
-function getEvents(word, page) {
-  fetchApi(word, page)
-    .then(data => {
-      const hits = data.data.hits;
-      ref.div.innerHTML = createMarckup(hits);
-      const i = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      i.refresh();
-
-      if (hits.length === 0) {
-        ref.btnMore.style.display = 'none';
-        return Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      if (hits.length > 1 && page === 1) {
-        ref.btnMore.style.display = 'block';
-        return Notiflix.Notify.success(
-          `Hooray! We found ${data.data.totalHits} images.`
-        );
-      }
-    })
-    .catch(err => {
-      console.log(err);
+async function getEvents(word, page) {
+  try {
+    const data = await fetchApi(word, page);
+    const hits = data.data.hits;
+    ref.div.innerHTML = createMarckup(hits);
+    const i = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
     });
+    i.refresh();
+
+    if (hits.length === 0) {
+      ref.btnMore.style.display = 'none';
+      return Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    if (hits.length > 1 && page === 1) {
+      ref.btnMore.style.display = 'block';
+      return Notiflix.Notify.success(
+        `Hooray! We found ${data.data.totalHits} images.`
+      );
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function LoadMore(word, page) {
-  fetchApi(word, page)
-    .then(data => {
-      ref.div.insertAdjacentHTML('beforeend', createMarckup(data.data.hits));
-      const i = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      i.refresh();
-      ref.hitsOnLoad += 40;
-      if (ref.hitsOnLoad >= data.data.totalHits) {
-        return Notiflix.Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    })
-    .catch(err => {
-      console.log(err);
+async function LoadMore(word, page) {
+  try {
+    const data = await fetchApi(word, page);
+    ref.div.insertAdjacentHTML('beforeend', createMarckup(data.data.hits));
+    const i = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
     });
+    i.refresh();
+    ref.hitsOnLoad += 40;
+    if (ref.hitsOnLoad >= data.data.totalHits) {
+      return Notiflix.Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 }
